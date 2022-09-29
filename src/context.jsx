8d6f90c253eccba325bbf0e2e7ba693794745d73
SaidNivek <kevin.vi.dias@gmail.com>
2022-09-28 22:51:10 -0400

    Get random and meal by name connected/fetched

--- src/context.jsx
+++ src/context.jsx
@@ -4,16 +4,20 @@ const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
 
-    useEffect(() => {
-        const fetchData = async() => {
-            try {
-                const response = await fetch('https://randomuser.me/api/')
-                const data = await response.json()
-                console.log(data)
-            } catch (error) {
-                console.log(error)
-            }
+    const fetchData = async() => {
+        try {
+            const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+            const data = await response.json()
+            const random = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+            const randomData = await random.json()
+            console.log(data)
+            console.log(randomData)
+        } catch (error) {
+            console.log(error)
         }
+    }
+
+    useEffect(() => { 
         fetchData()
     }, [])
     
