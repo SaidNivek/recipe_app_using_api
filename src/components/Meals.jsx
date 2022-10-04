dedbfde24e3560c1d28f6b21a650292704b36015
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-04 17:06:04 -0400

    Use thumbs up component for card, replacing the like text

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useGlobalContext } from '../context'
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 function Meals() {
 
@@ -8,13 +9,14 @@ function Meals() {
   return (
       <section className="section-center">
         {meals.map((singleMeal) => {
+          // Pull out the properties that we want to use from the meals object, and assign easier to read variable names, as needed
           const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
           return (
             <article key={idMeal} className="single-meal" >
-              <img src={image} style={{width:'200px'}} className="img" />
+              <img src={image} className="img" />
               <footer>
                 <h5>{title}</h5>
-                <button className="like-btn">click me</button>
+                <button className="like-btn"><BsHandThumbsUp /></button>
               </footer>
             </article>
           )
