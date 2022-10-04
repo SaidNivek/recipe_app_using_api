6f2fbe8159398e9ad9dd9893956a05a3568f1b2b
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-04 16:06:50 -0400

    Conver fetch data to axios and install axios

--- src/context.jsx
+++ src/context.jsx
@@ -1,27 +1,39 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios'
 
 const AppContext = React.createContext()
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a'
 
 const AppProvider = ({ children }) => {
 
-    const fetchData = async() => {
+    const fetchMeals = async(url) => {
         try {
-            const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-            const data = await response.json()
-            const random = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-            const randomData = await random.json()
-            console.log(data)
-            console.log(randomData)
-        } catch (error) {
-            console.log(error)
+            const response = await axios(url)
+            console.log(response)
+        }
+        catch (e) {
+            console.log(e.response)
         }
     }
+    
+    // React fetch API
+    // const fetchData = async() => {
+    //     try {
+    //         const response = await fetch(allMealsUrl)
+    //         const data = await response.json()
+    //         const random = await fetch(randomMealUrl)
+    //         const randomData = await random.json()
+    //         console.log(data)
+    //         console.log(randomData)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
     useEffect(() => { 
-        fetchData()
+        fetchMeals(allMealsUrl)
     }, [])
     
     return <AppContext.Provider value={{name: 'kevin', role:'student'}}>
