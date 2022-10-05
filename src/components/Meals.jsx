86d88c7cc1de0cafe24bf0310582ae7fd3001273
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-05 16:08:41 -0400

    Fix an error on the empty search.

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -13,7 +13,7 @@ function Meals() {
           <h4>Loading...</h4>
         </section>
       )
-    } else if(meals < 1) {
+    } else if(meals.length < 1) {
       return (
         <section className="section">
           <h4>No meals matched your search term. Please try again.</h4>
