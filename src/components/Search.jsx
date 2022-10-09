41f1d3be09bd0d5e0a821efacd390b9be45d3474
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-08 20:08:26 -0400

    Beging Search.jsc functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,22 @@ import React, { useState } from 'react'
 import { useGlobalContext } from '../context'
 
 function Search() {
+
+const [text, setText] = useState('')
+
+const handleChange = (e) => {
+  setText(e.target.value)
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+}
+
   return (
     <header className="search-container">
       <form>
-        <input className="form-input" type="text" placeholder='Search for a meal'></input>
-        <button className="btn" type="submit">Search</button>
+        <input className="form-input" type="text" placeholder='Search for a meal' onChange={handleChange}></input>
+        <button className="btn" type="submit" onSubmit={handleSubmit}>Search</button>
         <button className="btn btn-hipster" type="button">Surprise Me!</button>
       </form>
     </header>
