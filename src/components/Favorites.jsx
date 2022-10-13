3624d3ec68bcc93176d5136f89714e4e66e98f79
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-13 13:58:38 -0400

    Wrap favorites component return in a section

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -6,22 +6,23 @@ function Favorites() {
   const {favorites, selectMeal, removeFromFavorites } = useGlobalContext()
 
   return (
-    <div className="favorites-content">
-      <h5>Favorites</h5>
-      <div className="favorites-container">
-{favorites.map((item) => {
-  // This will destructure the item each time through the map to easier to use names (idMeal and image)
-  const { idMeal, strMealThumb: image, strMeal: title } = item
-
-  return (
-    <div key={idMeal} className="favorite-item">
-      <img src={image} alt={`image of ${title}`} className="favorites-img img" />
-      <button className="remove-btn" onClick={() => removeFromFavorites(idMeal)}></button>
-    </div>
-  )
-})}
+    <section className='favorites'>
+      <div className="favorites-content">
+        <h5>Favorites</h5>
+        <div className="favorites-container">
+        {favorites.map((item) => {
+          // This will destructure the item each time through the map to easier to use names (idMeal and image)
+          const { idMeal, strMealThumb: image, strMeal: title } = item    
+          return (
+            <div key={idMeal} className="favorite-item">
+              <img src={image} alt={`image of ${title}`} className="favorites-img img" />
+              <button className="remove-btn" onClick={() => removeFromFavorites(idMeal)}></button>
+            </div>
+            )
+          })}
+        </div>
       </div>
-    </div>
+  </section>
   )
 }
 
